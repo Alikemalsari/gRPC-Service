26a3f64e3e9309316554de58ff742f5fab037c77
Ali Kemal SARI <a.kemalsari46@gmail.com>
2024-04-28 22:10:32 +0300

    Commit

---
+++ GrpcService/GrpcService/Services/CustomerDb.cs
@@ -0,0 +1,8 @@
+﻿namespace GrpcService.Services
+{
+    public class CustomerDb
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ GrpcService/GrpcService/Services/CustomerService.cs
@@ -0,0 +1,16 @@
+﻿using Grpc.Core;
+
+using System.Threading.Tasks;
+
+namespace GrpcService.Services
+{
+    public class CustomerService :Greeter.GreeterBase
+    {
+        public override Task<CustomerResponse> GetCustomer(GetCustomerRequest request, ServerCallContext context)
+        {
+            // Burada müşteri bilgilerini veritabanından veya başka bir kaynaktan alabilirsiniz
+            // Örnek olarak sadece sabit bir değer döndürelim
+            return Task.FromResult(new CustomerResponse { Name = "John Doe", Email = "john@example.com" });
+        }
+    }
+}
